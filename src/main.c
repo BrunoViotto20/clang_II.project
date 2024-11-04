3784b2826bf95552f04803e74990abe8774a4e9d
BrunoViotto20 <Bruno.Santos03@cs.up.edu.br>
2024-11-04 19:44:49 -0300

    refactor: Make menu functions return the option directly instead of an option integer

--- src/main.c
+++ src/main.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Produto Produto;
-typedef struct FormaPagamento FormaPagamento;
+typedef struct Product Product;
+typedef struct PaymentMethod PaymentMethod;
 
-struct Produto
+struct Product
 {
-    char nome[32];
-    double preco;
+    char name[32];
+    double price;
 };
 
-struct FormaPagamento
+struct PaymentMethod
 {
-    char nome[32];
-    double custo;
+    char name[32];
+    double fee;
 };
 
-int main_menu();
-int payment_menu();
+Product products_menu(const Product *products, int length);
+PaymentMethod payment_menu(const PaymentMethod *paymentMethods, int length);
 
 int main(void)
 {
-    const Produto produtos[] = {
+    const Product products[] = {
         {"Cachorro Quente", 12},
         {"X-Salada", 15.5},
         {"X-Bacon", 18},
@@ -29,64 +29,66 @@ int main(void)
         {"Refrigerante", 4.5},
         {"Cerveja Brahma", 6},
         {"Cigarro Malboro", 6}};
+    const int productsLength = sizeof(products) / sizeof(Product);
 
-    const FormaPagamento formasPagamento[] = {
+    const PaymentMethod paymentMethods[] = {
         {"Crédito", .02},
         {"Débito", .02},
         {"Dinheiro", 0}};
+    const int paymentMethodsLength = sizeof(paymentMethods) / sizeof(Product);
 
-    int menu = main_menu();
-
-    Produto produto = produtos[menu - 1];
+    Product product = products_menu(products, productsLength);
 
     printf("Digite a quantidade do item: ");
-    int quantidade;
-    scanf("%d", &quantidade);
+    int amount;
+    scanf("%d", &amount);
 
-    double preco_total = quantidade * produto.preco;
+    PaymentMethod paymentMethod = payment_menu(paymentMethods, paymentMethodsLength);
 
-    int opcaoPagamento = payment_menu();
-
-    FormaPagamento formaPagamento = formasPagamento[opcaoPagamento - 1];
-
-    if (formaPagamento.custo != 0)
+    if (paymentMethod.fee != 0)
     {
-        printf("Taxa de %.1lf%%, sobre o valor da compra:\n", formaPagamento.custo * 100);
+        printf("Taxa de %.1lf%%, sobre o valor da compra:\n", paymentMethod.fee * 100);
     }
-    double preco_final = preco_total * (1 + formaPagamento.custo);
-    printf("O valor total é: R$%.2f\n", preco_final);
+    
+    double total_price = amount * product.price;
+    double final_price = total_price * (1 + paymentMethod.fee);
+    printf("O valor total é: R$%.2f\n", final_price);
 
     return 0;
 }
 
-int main_menu()
+Product products_menu(const Product *products, int length)
 {
     printf("MENU DE PEDIDOS:\n\n");
-    printf("1 - para Cachorro-quente R$ 12,00\n");
-    printf("2 - para X-salada R$ 15,50\n");
-    printf("3 - para X-bacon R$ 18,00\n");
-    printf("4 - para Torrada simples R$ 8,00\n");
-    printf("5 - para Refrigerante  R$ 4,50\n");
-    printf("6 - para Cerveja Brahma R$ 6,00\n");
-    printf("7 - para Cigarro Malboro R$ 6.00\n");
+    for (int i = 0; i < length; i++)
+    {
+        Product product = products[i];
+        printf("[ %d ] %s R$%.2lf\n", i + 1, product.name, product.price);
+    }
     printf("\n");
     printf("Digite a opção do produto: ");
-    int opcao;
-    scanf("%d", &opcao);
+    int option;
+    scanf("%d", &option);
 
-    return opcao;
+    return products[option - 1];
 }
 
-int payment_menu()
+PaymentMethod payment_menu(const PaymentMethod *paymentMethods, int length)
 {
     printf("MENU DE PAGAMENTO :\n\n");
-    printf("1 - para Cartão de Débito +2%%\n");
-    printf("2 - para Cartão de Credito +2%%\n");
-    printf("3 - para Dinheiro em Especie\n");
+    for (int i = 0; i < length; i++)
+    {
+        PaymentMethod payment_method = paymentMethods[i];
+        printf("[ %d ] %s", i + 1, payment_method.name);
+
+        if (payment_method.fee != 0) {
+            printf(" (+%.1lf%%)\n", payment_method.fee * 100);
+        }
+    }
     printf("\n");
     printf("Digite a opção da forma de pagamento: ");
-    int opcao;
-    scanf("%d", &opcao);
+    int option;
+    scanf("%d", &option);
 
-    return opcao;
+    return paymentMethods[option - 1];
 }
